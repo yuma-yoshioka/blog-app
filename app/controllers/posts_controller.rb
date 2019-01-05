2b385c433fdc20624b1528e80eb901fefd2758f3
yuma-yoshioka <yoshiokayuma@yoshiokayuumanoMacBook-puro.local>
2019-01-05 13:59:31 +0900

    describe_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+	
+	before_action :move_to_index, except: :index
+
 	def index
 		@posts = Post.all 
 	end
@@ -8,11 +11,37 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		Post.create(post_params)
+		Post.create(text: post_params[:text], user_id: current_user.id)
+	end
+
+	def destroy
+		post = Post.find(params[:id])
+      if post.user_id == current_user.id
+        post.destroy
+      end
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		post = Post.find(params[:id])
+		if post.user_id == current_user.id
+			post.update(post_params)
+		end
 	end
 
 	private
 	def post_params
 		params.permit(:text)
+	end
+
+	def move_to_index
+		redirect_to action: :index unless user_signed_in?
 	end
 end
